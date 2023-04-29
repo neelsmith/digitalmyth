a1a200423441211b61918e460c6a0805979de6eb
Neel Smith <dnsmith.neel@gmail.com>
2023-04-29 15:18:14 -0400

    Add schedule compilation

---
+++ compilecal.jl
@@ -0,0 +1,26 @@
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+using CourseCalendar
+
+calfile = joinpath(pwd(), "F23", "calendar1.toml")
+topics = joinpath(pwd(), "F23", "topics1.txt")
+sched = courseSchedule(calfile, topics)
+
+md = mdcalendar(sched)
+
+hdg = """---
+title: "Daily schedule"
+layout: page
+nav_order: 1
+has_children: true
+---
+
+## Daily schedule
+
+
+"""
+
+open(joinpath(pwd(), "docs", "schedule", "index.md"), "w") do io
+    write(io, hdg * md)
+end

8263894f8d784295cbedc496cf7b029d6d3d4aac
Neel Smith <dnsmith.neel@gmail.com>
2023-10-10 09:28:53 -0400

    More tutorial notebooks

--- pluto/topicmodels_guide.jl
+++ pluto/topicmodels_guide.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.27
+# v0.19.29
 
 using Markdown
 using InteractiveUtils
@@ -18,14 +18,37 @@ end
 # ╠═╡ show_logs = false
 begin
 	using Pkg
-	Pkg.add(url="https://github.com/neelsmith/CitableCorpusAnalysis.jl")
 	
-	using TopicModelsVB
+	Pkg.add(url="https://github.com/neelsmith/CitableCorpusAnalysis.jl")
 	using CitableCorpusAnalysis
+
+	Pkg.add("TopicModelsVB")
+	using TopicModelsVB
+
+	Pkg.add("CitableBase")
 	using CitableBase
+	Pkg.add("CitableCorpus")
 	using CitableCorpus
+	Pkg.add("Orthography")
 	using Orthography
+
+	Pkg.add("PlutoUI")
 	using PlutoUI
+
+	Pkg.add("Statistics")
+	using Statistics
+	Pkg.add("SplitApplyCombine")
+	using SplitApplyCombine
+
+	Pkg.add("TSne")
+	using TSne
+	
+	Pkg.add("PlotlyJS")
+	using PlotlyJS
+
+	#Pkg.add("TSne")
+	#using TSne
+	md"""(*Unhide this cell to see or modify your Julia environment*) """
 end
 
 # ╔═╡ 76656778-6698-11ee-09ce-a730fa578e8e
@@ -37,13 +60,15 @@ md"""
 """ 
 
 # ╔═╡ 7f1a8602-392f-431c-9059-d53d1c2d9cc1
-md"""*Number of topics*: $(@bind n confirm(Slider(2:20, default=12, show_value = true))) *Iterations* $(@bind iters confirm(Slider(100:50:1000, default=200, show_value = true)))
+md"""*Number of topics*: $(@bind n confirm(Slider(2:30, default=12, show_value = true))) *Iterations* $(@bind iters confirm(Slider(100:50:1000, default=200, show_value = true)))
 """
 
 # ╔═╡ 90b5a2ef-c8ab-4fb9-a12d-0832a9d4e9fb
 (n, iters)
 
 # ╔═╡ 9e43dba0-1ed9-441b-aca1-7bdadabae761
+"""Train a model on corpus `corp`.
+"""
 function modeltopics(corp, n)
 	model = fCTM(corp, n)
 	train!(model, tol = 0, checkelbo = Inf)
@@ -59,6 +84,14 @@ md"""
 md"""
 *Number of terms to show* $(@bind terms_n Slider(5:50, default=10, show_value = true))
 """
+
+# ╔═╡ 556457dc-bbbd-41a8-bf47-21b19ce3dec7
+thelayout = Layout(
+	title = "Plotted",
+	height = 300
+	
+	
+)
 
 # ╔═╡ 893a7b3d-102a-4c2c-98d1-d37a03d0fa8a
 html"""
@@ -116,6 +149,37 @@ begin
 	join(rows, "\n") |> Markdown.parse
 end
 
+# ╔═╡ 524866e0-499d-4aa8-98fd-4054840acfe8
+#tmcorpus(CitableTextCorpus(c.passages[1:n_psgs]), simpleAscii())
+
+# ╔═╡ 39a7935d-fb4b-4893-aad5-13abc205758d
+md"""> **Reduce data for visualization**"""
+
+# ╔═╡ 48a2ca09-b1f8-437f-8a39-09f9c68850d1
+"""Scale data for plotting with TSne.
+This is voodoo from the TSne docs.
+"""
+rescale(A; dims=1) = (A .- mean(A, dims=dims)) ./ max.(std(A, dims=dims), eps())
+
+# ╔═╡ e8e4a94f-3d02-4e30-9405-80de8e652001
+rescaled = rescale(model.beta, dims=1);
+
+# ╔═╡ 4b050ad7-476f-4fd3-ac6e-446fe5960932
+# ╠═╡ show_logs = false
+reduced = tsne(transpose(rescaled))
+
+# ╔═╡ ff379111-7817-4902-9f19-d4d8bb66a0b1
+xs = reduced[:, 1]
+
+# ╔═╡ f10919fa-4ab6-46f0-90ba-6a4d6412c462
+ys = reduced[:, 2]
+
+# ╔═╡ 218dede6-c21d-43de-850e-7d3bebe6a1d1
+Plot(
+	scatter(x = xs, y = ys, mode = "markers"),
+	thelayout
+)
+
 # ╔═╡ Cell order:
 # ╟─910bb052-82e1-4566-96eb-70d3789982bb
 # ╟─76656778-6698-11ee-09ce-a730fa578e8e
@@ -123,14 +187,23 @@ end
 # ╟─7f1a8602-392f-431c-9059-d53d1c2d9cc1
 # ╟─8eceb8d5-5e86-4b71-96d2-d78c94d2e552
 # ╠═90b5a2ef-c8ab-4fb9-a12d-0832a9d4e9fb
-# ╠═9e43dba0-1ed9-441b-aca1-7bdadabae761
+# ╟─9e43dba0-1ed9-441b-aca1-7bdadabae761
 # ╟─e295ca3b-7e89-41c6-9163-b2a85526ebf0
-# ╠═741b67b6-a185-4ac8-a442-6a25067de3f7
+# ╟─741b67b6-a185-4ac8-a442-6a25067de3f7
+# ╟─218dede6-c21d-43de-850e-7d3bebe6a1d1
 # ╟─645301aa-2870-44fd-a076-33d3a676e69b
 # ╟─4c2472a2-8d5f-4093-b925-4e8698e79ff6
+# ╠═556457dc-bbbd-41a8-bf47-21b19ce3dec7
 # ╟─893a7b3d-102a-4c2c-98d1-d37a03d0fa8a
 # ╟─89d9e7cd-a425-4afc-8403-b5469323e63c
 # ╟─f57b186d-1576-403f-86ea-2916678f9739
 # ╟─444060a8-c88a-45c5-b8c2-f97065c85888
 # ╠═38a31546-a376-4398-a185-fb751f769362
 # ╠═2db7e0d3-8f44-4e1e-9043-c97d97b5eac3
+# ╠═524866e0-499d-4aa8-98fd-4054840acfe8
+# ╟─39a7935d-fb4b-4893-aad5-13abc205758d
+# ╟─48a2ca09-b1f8-437f-8a39-09f9c68850d1
+# ╠═e8e4a94f-3d02-4e30-9405-80de8e652001
+# ╠═4b050ad7-476f-4fd3-ac6e-446fe5960932
+# ╠═ff379111-7817-4902-9f19-d4d8bb66a0b1
+# ╠═f10919fa-4ab6-46f0-90ba-6a4d6412c462
